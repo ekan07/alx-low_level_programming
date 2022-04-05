1c74b8ecb3dcb2aabe2eecda946132183ba31b25
ekan07 <aniekanbon@gmail.com>
2022-04-05 23:50:33 +0100

    modify files: 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,16 +2,12 @@
 #include <stdlib.h>
 
 /**
- * *str_concat - main function.
- * @s1: Primer string.
- * @s2: Segundo string.
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Description: Escriba una función que devuelva un puntero a
- * un espacio recién asignado en la memoria, que contiene
- * una copia de la cadena dada como parámetro.
- * Return: The pointer to str.
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	int size;
@@ -23,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (!s2)
 		s2 = "";
-	for (size = 0; s1[size] != '\0'; size++) 
+	for (size = 0; s1[size] != '\0'; size++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
