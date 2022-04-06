fe95e1a8c4e526013fc92a2a9a806432018230f9
ekan07 <aniekanbon@gmail.com>
2022-04-06 23:56:45 +0100

    Add files: 100-argsstostr.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,9 +12,11 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1)
 		return (NULL);
+
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
@@ -25,11 +27,10 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-	}
-	for (i = 0; i < height; i++)
-	{
+
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
+
 	return (grid);
 }
