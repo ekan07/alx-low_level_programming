7bca8b2a2470dcb27ad7bd015f4c0c3c98d3cae4
ekan07 <aniekanbon@gmail.com>
2022-04-08 03:00:40 +0100

    Modify files: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,54 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, j, s1_len, s2_len;
-    char *str;
+	unsigned int i, j, s1_len, s2_len;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    s1_len = 0;/* length of s1 */
-    while (*(s1 + s1_len) != '\0')
-        s1_len++;
-    s2_len = 0;/* length of s2 */
-    while (*(s2 + s2_len) != '\0')
-        s2_len++;
+	s1_len = 0;/* length of s1 */
+	while (*(s1 + s1_len) != '\0')
+		s1_len++;
+	s2_len = 0;/* length of s2 */
+	while (*(s2 + s2_len) != '\0')
+		s2_len++;
 
-    if (n >= s2_len)
-        str = malloc((s1_len + s2_len + 1) * sizeof(char));
-    else
-        str = malloc((s1_len + n + 1) * sizeof(char));
+	if (n >= s2_len)
+		str = malloc((s1_len + s2_len + 1) * sizeof(char));
+	else
+		str = malloc((s1_len + n + 1) * sizeof(char));
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    i = 0;
-    while (i < s1_len)
-    {
-        *(str + i) = *(s1 + i);
-        i++;
-    }
+	i = 0;
+	while (i < s1_len)
+	{
+		*(str + i) = *(s1 + i);
+		i++;
+	}
 
-    j = 0;
-    while (j < s2_len)
-    {
-        *(str + s1_len) = *(s2 + j);
-        s1_len++;
-        j++;
-    }
-    
-    *(str + (s1_len + 1)) = '\0';
-    return (str);
+	j = 0;
+	while (j < s2_len)
+	{
+		*(str + i) = *(s2 + j);
+		i++;
+		j++;
+	}
+
+	*(str + (i + 1)) = '\0';
+	return (str);
 }
