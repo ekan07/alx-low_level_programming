330806bd6222bdcbd90caa626090ee06f01e7f7e
ekan07 <aniekanbon@gmail.com>
2022-07-02 20:29:30 +0100

    Modify files: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!hash_table->array)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
