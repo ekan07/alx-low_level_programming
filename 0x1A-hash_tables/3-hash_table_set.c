c181023416d8db8238bafd4f4b60569c880c629e
ekan07 <aniekanbon@gmail.com>
2022-07-07 12:44:21 +0100

    Modify file: 3-hash_table_set.py

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int hash_index;
 	hash_node_t *new_hnode;
 
-	if (ht == NULL || strlen(key) == 0)
+	if (!ht || !key || !(*key) || !value)
 		return (0);
 
 	new_hnode = malloc(sizeof(hash_node_t));
